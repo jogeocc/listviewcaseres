03a2e52cccd0aa1e9743c73bc20c54e25cffb39a
jogeocc <jogeochanc@gmail.com>
2018-03-05 14:46:45 -0600

    Agregando principios de listview personalizado

---
+++ app/src/main/java/com/example/jgchan/listsviews/AlmacenContactos.java
@@ -0,0 +1,8 @@
+package com.example.jgchan.listsviews;
+
+/**
+ * Created by jgchan on 5/03/18.
+ */
+
+public class AlmacenContactos {
+}
---
+++ app/src/main/java/com/example/jgchan/listsviews/Contacto.java
@@ -0,0 +1,43 @@
+package com.example.jgchan.listsviews;
+
+import java.util.ArrayList;
+
+/**
+ * Created by jgchan on 5/03/18.
+ */
+
+public class Contacto {
+
+    private String nombre,telefono, correo;
+
+
+    public Contacto(String nombre, String telefono, String correo) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
